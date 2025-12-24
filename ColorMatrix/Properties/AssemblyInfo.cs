223e94185935c343cc18a2c49be200b0d3c86bb7
unknown <landenlabs@gmail.com>
2025-12-24 11:16:18 -0500

    update ms build

--- ColorMatrix/Properties/AssemblyInfo.cs
+++ ColorMatrix/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorMatrix")]
+[assembly: AssemblyTitle("Color Matrix")]
 [assembly: AssemblyDescription("Experiment with color matrix effects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LanDen Labs")]
+[assembly: AssemblyCompany("LanDen Labs - Dennis Lang")]
 [assembly: AssemblyProduct("ColorMatrix")]
-[assembly: AssemblyCopyright("Copyright © LanDen Labs 2010")]
-[assembly: AssemblyTrademark("Dennis Lang")]
+[assembly: AssemblyCopyright("Copyright © LanDen Labs 2025")]
+[assembly: AssemblyTrademark("free to use, can't sell")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,7 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5")]
 [assembly: NeutralResourcesLanguage("en")]
