c917a7e488febda97cf152b28439043bf63489fb
Dennis Lang <landenlabs2@gmail.com>
2025-12-28 13:02:12 -0500

    cleanup batch files

--- clean-dir.bat
+++ clean-dir.bat
@@ -7,11 +7,9 @@ call ..\dev-setup.bat
 cd %prog% 
  
 @echo ---- Clean Release %prog% 
-%msbuild% %prog%.sln /t:Clean
-%msbuild% %prog%.csproj /t:Clean
+"%msbuild%" "%prog%.sln" /t:Clean
 lldu -sum obj bin 
 rmdir /s obj  2> nul
 rmdir /s bin  2> nul
-@rem %msbuild% %prog%.sln  -t:Clean
 
 cd ..
